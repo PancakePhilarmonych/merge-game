f19f279e9394a26028446854079fa5f7ef0f1e42
PancakePhilarmonych <rikko.alexandr@gmail.com>
2023-06-26 22:04:44 +0400

    refactor: improve Row

--- src/modules/Row.ts
+++ src/modules/Row.ts
@@ -1,26 +1,30 @@
 import Tile from "./Tile";
-import * as PIXI from 'pixi.js'
+import * as PIXI from 'pixi.js';
 
 export default class Row {
   private container: PIXI.Container;
   private rowIndex: number;
-  public tiles: Tile[];
+  private tiles: Tile[];
 
-  constructor(rowIndex: number, ) {
-    this.rowIndex = rowIndex;
+  constructor(index: number) {
+    this.rowIndex = index;
     this.container = new PIXI.Container();
     this.tiles = [];
   }
 
-  init(size: number, tileSize: number) {
+  public init(size: number, tileSize: number): void {
     for (let tileIndex = 0; tileIndex < size; tileIndex++) {
       const tile = new Tile(tileIndex, this.rowIndex, tileSize, 0);
       this.tiles.push(tile);
-      this.container.addChild(tile.getSpite());
+      this.container.addChild(tile.getContainer());
     }
   }
 
-  getContainer() {
+  public getContainer(): PIXI.Container {
     return this.container;
+  }
+
+  public getTiles(): Tile[] {
+    return this.tiles;
   }
 }
