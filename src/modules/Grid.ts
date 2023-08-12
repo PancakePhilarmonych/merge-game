7d75b22b4da2919c7929642d65702a6782334c84
PancakePhilarmonych <rikko.alexandr@gmail.com>
2023-08-13 02:36:11 +0400

    refactoring: unused container && methods

--- src/modules/Grid.ts
+++ src/modules/Grid.ts
@@ -3,71 +3,57 @@ import Tile from "./Tile";
 
 export default class Grid {
   private cells: Tile[][];
-  private container: PIXI.Container;
-  private selectedTile: Tile | null = null;
-  public tileSize: number;
+  private selected: Tile | null = null;
+  private cellSize: number;
 
-  constructor(rowsCount: number, boardSize: number) {
+  constructor(rowsCount: number, boardWidth: number) {
     this.cells = [];
-
-    this.tileSize = boardSize / rowsCount;
-
-    this.container = new PIXI.Container();
-    this.container.sortableChildren = true;
-    this.container.interactiveChildren = true;
-
-    this.initRows(rowsCount, this.tileSize);
-    this.createGrid();
+    this.cellSize = boardWidth / rowsCount;
+    this.initRows(rowsCount);
   }
 
-  private initRows(rowsCount: number, tileSize: number): void {
+  private initRows(rowsCount: number): void {
     for (let row = 0; row < rowsCount; row++) {
       this.cells[row] = [];
 
       for (let col = 0; col < rowsCount; col++) {
-        const tile = new Tile(col, row, tileSize);
-        this.cells[row][col] = tile;
+        const cell = new Tile(col, row, this.cellSize);
+        this.cells[row][col] = cell;
       }
     }
   }
 
-  private createGrid() {
-    this.container.addChild(...this.getSprites());
-  }
-
   getSprites(): PIXI.Sprite[] {
-    return this.cells.map((row: Tile[]) => row.map((tile: Tile) => tile.sprite)).flat();
+    return this.cells.map((row: Tile[]) => row.map((cell: Tile) => cell.sprite)).flat();
   }
 
-  getTiles(): Tile[] {
+  getCells(): Tile[] {
     return this.cells.flat();
   }
 
-  getContainer(): PIXI.Container {
-    return this.container;
+  public getCell(x: number, y: number): Tile | null {
+    return this.cells?.[x]?.[y] ?? null;
   }
 
-  public getTile(x: number, y: number): Tile | null {
-    return this.cells[y] && this.cells[y][x] ? this.cells[y][x] : null;
-  }
-
-  public selectTile(tile: Tile): void {
-    if (this.selectedTile) {
+  public select(cell: Tile): void {
+    if (this.selected) {
       this.deselect();
     }
 
-    this.selectedTile = tile;
-    // this.selectedTile.select();
+    this.selected = cell;
   }
 
   public deselect(): void {
-    if (this.selectedTile) {
-      // this.selectedTile.deselect();
-      this.selectedTile = null;
+    if (this.selected) {
+      this.selected = null;
     }
   }
 
-  getSelectedTilePosition(x: number, y: number): { x: number; y: number } {
+  getCellSize(): number {
+    return this.cellSize;
+  }
+
+  getSelectedCellPosition(x: number, y: number): { x: number; y: number } {
     return {
       x: Math.floor(x / this.cells[x][y].position.x),
       y: Math.floor(y / this.cells[x][y].position.y),
