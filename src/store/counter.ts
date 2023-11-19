c63742dfea86c84614695e284670a56d11a94bc0
PancakePhilarmonych <rikko.alexandr@gmail.com>
2023-11-19 19:58:47 +0400

    feat: add new methods to store

--- src/store/counter.ts
+++ src/store/counter.ts
@@ -1,15 +1,24 @@
 import { defineStore } from 'pinia'
+import { GameObject } from 'modules/GameObject'
+import { Colors, ColorsTextMap } from '../utils'
 
 export const useCounterStore = defineStore('counter', {
   state: () => {
-    return { count: 0 }
+    return { count: 0, color: Colors.EMPTY }
   },
   actions: {
-    increment() {
-      this.count++
+    increment(n?: number) {
+      n ? this.count += n : this.count++
     },
     reset() {
       this.count = 0
+    },
+    select(object: GameObject | null) {
+      if(!object) {
+        this.color = Colors.EMPTY;
+        return;
+      }
+      this.color = object.getColor();
     }
   },
 })
