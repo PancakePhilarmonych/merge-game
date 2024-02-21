79afe461cbcd916d800cbea1f63d3001ca57b392
PancakePhilarmonych <rikko.alexandr@gmail.com>
2024-02-22 01:24:41 +0400

    feat(GameManager): TOO BIG FOR COMMENT

--- src/modules/GameManager.ts
+++ src/modules/GameManager.ts
@@ -8,15 +8,23 @@ import App from './App';
 import { addAppListeners } from '../utils';
 
 export default class GameManager {
+  private app: App;
   private store: any;
   private grid: Grid;
   private gameObjects: GameObject[] = [];
   private hoveredCell: Tile | null = null;
   private selectedObject: GameObject | null = null;
+  private relaxMode = true;
+  private timer = Date.now();
+  private overallTime = 0;
+  private pause = false;
+  private secondsPerMove = 3;
   private container: PIXI.Container = new PIXI.Container();
+  private restartContainer: PIXI.Container = new PIXI.Container();
 
   constructor(counterStore: any) {
-    const { instance } = new App();
+    this.app = new App();
+    const instance = this.app.instance;
     addAppListeners(instance);
     this.store = counterStore;
     this.container.eventMode = 'dynamic';
@@ -26,11 +34,23 @@ export default class GameManager {
     const cellselectArea = this.grid.getContainers();
     this.container.addChild(...cellselectArea);
 
+    this.setListeners();
+
+    this.generateGameObjects();
+    instance.stage.addChild(this.container);
+    instance.stage.hitArea = instance.screen;
+
+    this.createStartContainer();
+    this.createRestartContainer();
+  }
+
+  private setListeners(): void {
     this.container
       .on('pointermove', this.moveOverContainer, this)
       .on('pointerdown', this.moveOverContainer, this);
 
     this.container.on<any>('select', (go: GameObject) => {
+      if (this.pause) return;
       if (this.selectedObject === go) return;
       this.selectedObject = go;
       this.store.select(go);
@@ -47,51 +67,247 @@ export default class GameManager {
       if (!this.hoveredCell) return;
       this.setObjectToCell(gameObject, this.hoveredCell);
     });
+  }
 
-    this.generateGameObjects();
-    instance.stage.addChild(this.container);
-    instance.stage.hitArea = instance.screen;
+  private createStartContainer(): void {
+    this.container.eventMode = 'none';
+    const startContainer = new PIXI.Container();
+    startContainer.zIndex = 100;
+    startContainer.width = this.app.instance.view.width;
+    startContainer.height = this.app.instance.view.height;
 
-    instance.ticker.add(() => {
-      const cellSize = this.grid.getCellSize();
-      const cells = this.grid.getCells();
+    const startBackground = new PIXI.Graphics();
+    startBackground.beginFill(0x2ecc71, 0.9);
+    startBackground.drawRect(0, 0, this.app.instance.view.width, this.app.instance.view.height);
+    startBackground.endFill();
 
-      if (!this.selectedObject) {
+    const startButton = new PIXI.Graphics();
+    startButton.beginFill(0xffffff, 1);
+    startButton.drawRoundedRect(
+      0,
+      0,
+      this.app.instance.view.width / 2,
+      this.app.instance.view.height / 6,
+      5,
+    );
+    startButton.endFill();
+    startButton.zIndex = 101;
+    // startButton.lineStyle(2, 0xffffff);
+    startButton.x = this.app.instance.view.width / 2 - startButton.width / 2;
+    startButton.y = this.app.instance.view.height / 2 - startButton.height / 2;
+    startButton.eventMode = 'dynamic';
+    startButton.cursor = 'pointer';
+
+    const startText = new PIXI.Text('Start', {
+      fill: 0x000000,
+      fontSize: startButton.height / 2,
+      fontWeight: 'bold',
+      align: 'center',
+    });
+    startText.zIndex = 102;
+
+    startText.x = startButton.x + startButton.width / 2 - startText.width / 2;
+    startText.y = startButton.y + startButton.height / 2 - startText.height / 2;
+
+    startContainer.addChild(startBackground);
+    startContainer.addChild(startButton);
+    startContainer.addChild(startText);
+    startContainer.visible = true;
+
+    this.app.instance.stage.addChild(startContainer);
+
+    startButton.on('pointerdown', () => {
+      const instance = this.app.instance;
+      this.app.instance.stage.removeChild(startContainer);
+      this.container.eventMode = 'dynamic';
+      instance.ticker.add(() => {
+        // Spritefall
+        // ------------------------------------------------------------------------------------------------
+        if (this.relaxMode) {
+          if (Date.now() - this.timer < this.secondsPerMove * 1000) {
+            if (this.pause) return;
+
+            const secondsToMillis = this.secondsPerMove * 1000;
+            const timeLeft = secondsToMillis - (Date.now() - this.timer);
+            const houndredPercent = secondsToMillis / 100;
+            const percent = timeLeft / houndredPercent;
+            this.store.updateTimerBar(percent);
+          } else {
+            this.overallTime++;
+            this.overallTime % 10 === 0 && this.secondsPerMove > 1 && this.secondsPerMove--;
+            this.timer = Date.now();
+
+            const allEmptyCells = this.grid
+              .getCells()
+              .filter((cell: Tile) => cell.getGameObject() === null);
+
+            if (allEmptyCells.length === 0) {
+              console.log('Game over');
+            } else {
+              const firstRandomEmptyCell =
+                allEmptyCells[Math.floor(Math.random() * allEmptyCells.length)];
+
+              const firstRandomColor = getRandomColor(true);
+
+              this.addNewObject(firstRandomEmptyCell, firstRandomColor);
+
+              if (allEmptyCells.length > 1) {
+                const secondRandomEmptyCell = allEmptyCells.filter(
+                  (cell: Tile) => cell !== firstRandomEmptyCell,
+                )[Math.floor(Math.random() * (allEmptyCells.length - 1))];
+
+                const secondRandomColor = getRandomColor(true);
+                this.addNewObject(secondRandomEmptyCell, secondRandomColor);
+              }
+            }
+          }
+        }
+        // ------------------------------------------------------------------------------------------------
+
+        const cellSize = this.grid.getCellSize();
+        const cells = this.grid.getCells();
+
+        if (cells.every((cell: Tile) => cell.getGameObject() !== null)) {
+          this.restartContainer.visible = true;
+          this.pause = true;
+
+          const scoreText = new PIXI.Text(`Score: ${this.store.count}`, {
+            fill: 0xffffff,
+            fontSize: 40,
+            align: 'center',
+          });
+
+          scoreText.x = this.app.instance.view.width / 2 - scoreText.width / 2;
+          scoreText.y = this.app.instance.view.height / 2 - scoreText.height / 2 - 100;
+
+          this.restartContainer.removeChild(this.restartContainer.children[3]);
+          this.restartContainer.addChild(scoreText);
+
+          if (this.selectedObject) {
+            this.moveObjectToOwnCell(this.selectedObject);
+            const selectedCell = this.selectedObject?.getCell();
+            selectedCell!.selectArea.alpha = 0;
+            this.container.removeAllListeners();
+            this.selectedObject = null;
+            this.store.select(null);
+          }
+
+          setTimeout(() => {
+            instance.ticker.stop();
+            this.container.eventMode = 'none';
+            // this.container.removeAllListeners();
+          }, 500);
+        }
+
+        if (!this.selectedObject) return;
+
+        const spriteSizeWidthAnchor = this.selectedObject.sprite.x;
+        const spriteSizeHeightAnchor = this.selectedObject.sprite.y;
+
         cells.forEach((cell: Tile) => {
+          const isHovered =
+            this.selectedObject &&
+            Math.floor(spriteSizeWidthAnchor / cellSize) === cell.position.x &&
+            Math.floor(spriteSizeHeightAnchor / cellSize) === cell.position.y;
+
+          if (isHovered) {
+            this.hoveredCell = cell;
+
+            cell.getGameObject() !== this.selectedObject
+              ? gsap.to(cell.sprite, { alpha: 0.8, duration: 0.8 })
+              : gsap.to(cell.sprite, { alpha: 1, duration: 0.1 });
+            return;
+          }
+
+          const selectedOjectCell = this.selectedObject!.getCell();
+
+          if (selectedOjectCell) {
+            selectedOjectCell.selectArea.alpha = 0.9;
+            selectedOjectCell.selectArea.zIndex = 2;
+          }
+
           cell.sprite.alpha = 1;
+          cell.selectArea.alpha = 0;
+          cell.selectArea.zIndex = 1;
         });
-        return;
-      }
-
-      const spriteSizeWidthAnchor = this.selectedObject.sprite.x;
-      const spriteSizeHeightAnchor = this.selectedObject.sprite.y;
-
-      cells.forEach((cell: Tile) => {
-        const isHovered =
-          this.selectedObject &&
-          Math.floor(spriteSizeWidthAnchor / cellSize) === cell.position.x &&
-          Math.floor(spriteSizeHeightAnchor / cellSize) === cell.position.y;
-
-        if (isHovered) {
-          this.hoveredCell = cell;
-
-          cell.getGameObject() !== this.selectedObject
-            ? gsap.to(cell.sprite, { alpha: 0.8, duration: 0.8 })
-            : gsap.to(cell.sprite, { alpha: 1, duration: 0.1 });
-          return;
-        }
-
-        const selectedOjectCell = this.selectedObject!.getCell();
-
-        if (selectedOjectCell) {
-          selectedOjectCell.selectArea.alpha = 0.9;
-          selectedOjectCell.selectArea.zIndex = 2;
-        }
-
-        cell.sprite.alpha = 1;
-        cell.selectArea.alpha = 0;
-        cell.selectArea.zIndex = 1;
       });
+
+      this.timer = Date.now();
+    });
+  }
+
+  private createRestartContainer(): void {
+    this.restartContainer = new PIXI.Container();
+    this.restartContainer.zIndex = 100;
+    this.restartContainer.width = this.app.instance.view.width;
+    this.restartContainer.height = this.app.instance.view.height;
+
+    const pauseBackground = new PIXI.Graphics();
+    pauseBackground.beginFill(0xff7675, 0.9);
+    pauseBackground.drawRect(0, 0, this.app.instance.view.width, this.app.instance.view.height);
+    pauseBackground.endFill();
+
+    const restartButton = new PIXI.Graphics();
+    restartButton.beginFill(0xffffff, 1);
+    restartButton.drawRoundedRect(
+      0,
+      0,
+      this.app.instance.view.width / 2,
+      this.app.instance.view.height / 6,
+      5,
+    );
+    restartButton.endFill();
+    restartButton.zIndex = 101;
+    // restartButton.lineStyle(2, 0xffffff);
+    restartButton.x = this.app.instance.view.width / 2 - restartButton.width / 2;
+    restartButton.y = this.app.instance.view.height / 2 - restartButton.height / 2;
+    restartButton.eventMode = 'dynamic';
+    restartButton.cursor = 'pointer';
+
+    const restartText = new PIXI.Text('Restart', {
+      fill: 0x000000,
+      fontSize: restartButton.height / 2,
+      fontWeight: 'bold',
+      align: 'center',
+    });
+    restartText.zIndex = 102;
+
+    restartText.x = restartButton.x + restartButton.width / 2 - restartText.width / 2;
+    restartText.y = restartButton.y + restartButton.height / 2 - restartText.height / 2;
+
+    this.restartContainer.addChild(pauseBackground);
+    this.restartContainer.addChild(restartButton);
+    this.restartContainer.addChild(restartText);
+    this.restartContainer.visible = false;
+
+    this.app.instance.stage.addChild(this.restartContainer);
+
+    restartButton.on('pointerdown', () => this.restartGame());
+  }
+
+  private addNewObject(cell: Tile, color: Colors): void {
+    const newGameObject = new GameObject(
+      cell.position.x,
+      cell.position.y,
+      cell.sprite.width,
+      color,
+    );
+
+    this.gameObjects.push(newGameObject);
+    this.container.addChild(newGameObject.sprite);
+    cell.setGameObject(newGameObject);
+    newGameObject.sprite.eventMode = 'none';
+
+    gsap.from(newGameObject.sprite, {
+      alpha: 0.0,
+      duration: 0.3,
+      ease: 'power2.out',
+      y: cell.sprite.y - 40,
+
+      onComplete: () => {
+        newGameObject.setCell(cell);
+        newGameObject.sprite.eventMode = 'dynamic';
+      },
     });
   }
 
@@ -223,14 +439,25 @@ export default class GameManager {
   }
 
   public restartGame(): void {
+    this.setListeners();
+
     this.gameObjects.forEach((gameObject: GameObject) => {
       gameObject.sprite.destroy();
     });
+
+    this.timer = Date.now();
+    this.overallTime = 0;
+    this.relaxMode = true;
+    this.secondsPerMove = 3;
+
+    this.selectedObject = null;
+    this.hoveredCell = null;
 
     this.gameObjects = [];
     this.grid.getCells().forEach((cell: Tile) => {
       cell.removeGameObject();
       cell.selectArea.alpha = 0;
+      cell.sprite.alpha = 1;
     });
 
     this.hoveredCell = null;
@@ -238,6 +465,10 @@ export default class GameManager {
 
     this.store.reset();
     this.generateGameObjects();
+    this.container.eventMode = 'dynamic';
+    this.restartContainer.visible = false;
+    this.pause = false;
+    this.app.instance.ticker.start();
   }
 
   private setNewColorToObject(object: GameObject): void {
