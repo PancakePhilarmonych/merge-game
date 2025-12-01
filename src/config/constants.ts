7d490ba08f2a012e572a114110d1e604b52274d9
PancakePhilarmonych <rikko.alexandr@gmail.com>
2025-12-01 23:41:52 +0400

    refactoring: change rows count

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
-export const ROWS_COUNT = 5;
+export const ROWS_COUNT = 4;
 export const INITIAL_TILES_COUNT = Math.floor(ROWS_COUNT * ROWS_COUNT * 0.7);
 export const GAME_DURATION = 20;
 export const NEW_OBJECT_DELAY = 300;
